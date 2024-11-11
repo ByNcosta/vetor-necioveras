1f34407bb9843eb93c9aca30741ae5a547214c6f
ByNcosta <nonatocosta212@gmail.com>
2024-11-11 15:00:35 -0300

    Update vetor.c

--- vetor.c
+++ vetor.c
@@ -1,47 +1,33 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    int *vetor;
+    size_t i;  // Alterado para size_t para representar índices e tamanho de forma adequada
+    size_t tamanho;
 
-int * criar_vetor(int tam){
-
-	int *vet; 
-
-	vet = (int *) malloc( sizeof (int) * tam);
-	return vet;
-
-}
-
-void ler_vetor (int *vet, int tam){
-
-	for(int i = 0; i < tam; i++)
-		scanf("%d", &vet[i]);
-
-}
-
-void exibir_vetor(int *vet, int tam){
-	printf("{");
-    for (int i = 0; i < tam; i++){
-		if (i < tam-1)
-            printf("%.2d ", vet[i]);
-        else
-            printf("%.2d", vet[i]);
+    // Verifica se o número de argumentos é suficiente
+    if (argc < 2) {
+        printf("Erro: número insuficiente de argumentos\n");
+        //return 1;
     }
-    printf("}\n");
-
+    // Lê o tamanho do vetor (agora tamanho é do tipo size_t)
+    tamanho = (size_t)atoi(argv[1]);
+    if (tamanho == 0) {
+        printf("Erro: tamanho inválido\n");
+        //return 1;
+    }
+    // Aloca a memória para o vetor (com o tipo size_t)
+    vetor = (int*)malloc(tamanho * sizeof(int));
+    if (vetor == NULL) {
+        printf("Erro: falha na alocação de memória\n");
+        //return 1;
+    }
+    for (i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+    // Libera a memória alocada
+    free(vetor);
+    return 0;
 }
 
-void destruir_vetor(int *vet){
-	free(vet);
-}
-
-
-int main(){
-
-	int tam; 
-	int *vet; 
-    scanf("%d", &tam);
-	vet = criar_vetor(tam);
-	ler_vetor(vet, tam);
-	exibir_vetor(vet, tam);
-	destruir_vetor(vet);
-
-}
